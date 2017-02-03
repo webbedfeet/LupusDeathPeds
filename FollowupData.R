9e5afbc16358ed5e3eac4aa7457cadc1620d0f5c
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2017-02-03 15:53:21 -0500

    removed male filter

--- FollowupData.R
+++ FollowupData.R
@@ -1,15 +1,15 @@
 # Follow-up data
 
-source('preamble.R')
 source('lib/reload.R')
 reload()
+source('preamble.R')
 load('data/rda/final_study_info.rda')
 load('data/rda/KM2IPD.rda')
 load('data/rda/summaries2IPD.rda')
 
 ids <- setdiff(study_info$pubID, c(names(KM2IPD), names(summaries2IPD)))
 
-fup_data <- study_info %>% filter(pubID %in% ids) %>% filter(male.only=='N') %>%
+fup_data <- study_info %>% filter(pubID %in% ids) %>%
   filter(pubID != 'Rodriguez_2000') %>% # See Notes.Rmd
   mutate(max.f.up = ifelse(is.na(max.f.up), end_of_study-start_of_study, max.f.up/12)) %>% # Convert to years
   # select(armID,pubID, max.f.up, number, deaths, f.up.months, mean.median) %>%

8a0e8ce866928b4b5119171045283da2f4721776
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-03-03 11:38:42 -0500

    Added plots for developed

--- MovingAvgResults.R
+++ MovingAvgResults.R
@@ -4,28 +4,40 @@ reload()
 load('data/rda/window_membership.rda')
 load('data/rda/study_info.rda')
 
-pdf(file = 'graphs/pedsMA.pdf')
-plt <- 'rda/peds' %>% mcmcout() %>%
-  collapseResults() %>%
-  pltResults()
-print(plt)
-
-dev.off()
 
 '%!in%' <- Negate('%in%')
 pdf(file = 'graphs/pedsdevelopingMA.pdf')
-results <- 'peds_developing' %>% mcmcout() %>%
+plt <- 'peds_developing' %>% mcmcout() %>%
   collapseResults() %>% filter(Dev=='Developed') %>%
   mutate(Dev = 'Developing countries') %>%
-  mutate_each(funs(ifelse(yr %in% c(1988,1989),NA, .)), LB, Med, UB)
-pltResults(results)
+  mutate_each(funs(ifelse(yr %in% c(1988,1989),NA, .)), LB, Med, UB) %>%
+  pltResults()
+print(plt)
 dev.off()
 
 pdf(file = 'graphs/pedsdevelopingMA_10.pdf')
-results <- 'peds_developing_10' %>% mcmcout() %>%
+plt <- 'peds_developing_10' %>% mcmcout() %>%
   collapseResults() %>% filter(Dev=='Developed') %>%
   mutate(Dev = 'Developing countries') %>%
-  mutate_each(funs(ifelse(yr %in% c(1988,1989),NA, .)), LB, Med, UB)
-pltResults(results)
+  mutate_each(funs(ifelse(yr %in% c(1988,1989),NA, .)), LB, Med, UB) %>%
+  pltResults()
+print(plt)
 dev.off()
 
+pdf(file = 'graphs/pedsdevelopedMA.pdf')
+plt <- 'peds_developed' %>% mcmcout() %>%
+  collapseResults() %>%
+  filter(Dev=='Developed') %>%
+  mutate(Dev = 'Developed countries') %>%
+  pltResults()
+print(plt)
+dev.off()
+
+pdf(file = 'graphs/pedsdevelopedMA_10.pdf')
+plt <- 'peds_developed_10' %>% mcmcout() %>%
+  collapseResults() %>%
+  filter(Dev=='Developed') %>%
+  mutate(Dev = 'Developed countries') %>%
+  pltResults()
+print(plt)
+dev.off()

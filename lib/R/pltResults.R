99c0c5a7fbb9c8b0c3c076cf47a0f66f73ad6ddf
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2017-03-29 16:52:29 -0400

    Specified breaks, removed color in pltResults

--- lib/R/pltResults.R
+++ lib/R/pltResults.R
@@ -16,8 +16,9 @@ pltResults <- function(bl){
   levels(bl$Year) <- gsub('s','',levels(bl$Year))
   p=ggplot(bl,aes(x=yr,y=Med, group=Year,color=Year))+geom_line(size=1)+
     geom_line(aes(x=yr, y=LB),linetype=2)+geom_line(aes(x=yr, y=UB),linetype=2)+
-    facet_wrap(~Dev, ncol=1)+ylim(0,1)+scale_color_hue(l=40)+
-    labs(x='Year',y='Probability of survival',color='')+
+    facet_wrap(~Dev, ncol=1)+ylim(0,1)+
+    scale_x_continuous('Year', breaks = seq(1960,2010, by=10))+
+    labs(y='Probability of survival',color='')+
     theme_bw()+theme(legend.key=element_blank(), legend.position=c(0.8,0.2),
                      legend.background=element_rect(fill="transparent"))
   return(p)
